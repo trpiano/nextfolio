2f1634bfe8e4a91f3e2bf09782d2541ad512f93e
Timóteo Reinheimer Piano <timoteopiano@gmail.com>
2023-07-05 14:53:31 -0300

    fix: GA tag import in the code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,15 +5,12 @@ import { Header } from "../components/pages/Header";
 import { AppContextProvider } from "../context/AppContext";
 import { TranslateContextProvider } from "../context/TranslateContext";
 
-import GoogleAnalytics from "../components/GoogleAnalytics/googleAnalytics";
-
 import { GlobalStyle } from "../styles/global";
 import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GoogleAnalytics GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_TRACKING_ID ?? ''} />
         <GlobalStyle />
         <TranslateContextProvider>
           <AppContextProvider>
